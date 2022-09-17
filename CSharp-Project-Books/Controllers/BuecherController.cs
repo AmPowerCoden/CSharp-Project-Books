cfc0391f80756511274d822b0374eb1a3b9c4784
Mario <mario.eubel@hotmail.de>
2022-09-17 14:31:52 +0200

    [Books-master]: Einfügen der Index Datei und des Models --> Erste Funktionalität sichtbar

--- CSharp-Project-Books/Controllers/BuecherController.cs
+++ CSharp-Project-Books/Controllers/BuecherController.cs
@@ -1,6 +1,7 @@
 ﻿using CSharp_Project_Books.Utils;
 using Microsoft.AspNetCore.Mvc;
 using BuecherDB;
+using CSharp_Project_Books.Models;
 
 namespace CSharp_Project_Books.Controllers
 {
@@ -18,7 +19,12 @@ namespace CSharp_Project_Books.Controllers
             string connectionString = GetConnectionString();
             var repository = new BuecherRepository(connectionString);
 
-            return View();
+            List<BuchDTO>? aktuelleBuecher = repository.HoleAktuelleBuecher();
+            List<BuchDTO>? archivierteBuecher = repository.HoleArchivierteBuecher();
+
+            var model = new BuecherListModel(aktuelleBuecher, archivierteBuecher);
+
+            return View(model);
         }
 
         public string GetConnectionString()
--- CSharp-Project-Books/Models/BuecherListModel.cs
+++ CSharp-Project-Books/Models/BuecherListModel.cs
@@ -1,9 +1,25 @@
-﻿namespace CSharp_Project_Books.Models
+﻿using BuecherDB;
+
+namespace CSharp_Project_Books.Models
 {
     public class BuecherListModel
     {
-        public BuecherListModel()
+
+        public List<BuchDTO> AktuelleBuecherList { get; set; } = new();
+
+        public List<BuchDTO> ArchivierteBuecherList { get; set; } = new();
+
+        public BuecherListModel(IEnumerable<BuchDTO> aktuelleBuecher, IEnumerable<BuchDTO> archivierteBuecher)
         {
+            foreach(BuchDTO buchDTO in aktuelleBuecher)
+            {
+                AktuelleBuecherList.Add(buchDTO);
+            }
+
+            foreach (BuchDTO buchDTO in archivierteBuecher)
+            {
+                ArchivierteBuecherList.Add(buchDTO);
+            }
         }
     }
 }
