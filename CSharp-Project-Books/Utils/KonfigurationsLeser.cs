74b6edc71be3f7ff27781b609aed5e4ee6c1d3a2
Mario <mario.eubel@hotmail.de>
2022-09-17 13:13:58 +0200

    [Books-master]: Einfuegen des KonfigurationsLeser

---
+++ CSharp-Project-Books/Utils/KonfigurationsLeser.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp_Project_Books.Utils
+{
+    public class KonfigurationsLeser : IKonfigurationsLeser
+    {
+        private readonly IConfiguration _configuration;
+
+        public KonfigurationsLeser(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public string LiesDatenbankVerbindungZurMariaDB()
+        {
+            return _configuration.GetConnectionString("MariaDB");
+        }
+    }
+
+    public interface IKonfigurationsLeser
+    {
+        string LiesDatenbankVerbindungZurMariaDB();
+    }
+}
